58ecedaa09e0127d5fa06d8236dc38f1bc953c7b
ak12033 <anush20303@gmail.com>
2025-07-17 17:40:22 +0530

    vercel fix

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ import vehicleRouter from './routes/vehicleRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 
 const app = express()
-const PORT = process.env.PORT || 5000;
 
 await connectDB()
 
@@ -18,4 +17,5 @@ app.get('/', (req, res) => res.send("Server is Live!"));
 app.use('/api', vehicleRouter);
 app.use('/api', bookingRouter);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
